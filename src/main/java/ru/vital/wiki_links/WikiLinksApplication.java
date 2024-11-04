5748ff6839eb88b0f774db683cd8dc22cfc83929
vital <govorirk@gmail.com>
2024-11-04 21:55:35 +0300

    init

---
+++ src/main/java/ru/vital/wiki_links/WikiLinksApplication.java
@@ -0,0 +1,13 @@
+package ru.vital.wiki_links;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiLinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vital/wiki_links/WikiLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vital.wiki_links;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiLinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
